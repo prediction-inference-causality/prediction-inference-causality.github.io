c3d14013af6f0d373a44a5fe6b5c2fd643b718da
davidahirshberg <david.a.hirshberg@gmail.com>
2024-07-04 21:46:32 -0400

    removed disused files from quarto extensions

--- lectures/_extensions/davidahirshberg/svgzoom/svg-as-object.lua
+++
@@ -1,5 +0,0 @@
-function Image (elem)
-  if elem.src:match('%.svg$') or elem.src:match('data:image/svg%+xml;') then
-    return pandoc.RawInline('html', string.format('<object type="image/svg+xml" data="%s" class="img"></object>', elem.src))
-  end
-end
