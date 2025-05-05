9e9a971f37a5e8381b0ffeb68aad7e399a646098
davidahirshberg <david.a.hirshberg@gmail.com>
2025-05-05 17:00:08 -0400

    Built site for gh-pages

--- lectures/_extensions/davidahirshberg/tldreveal/tldreveal-wrapper.js
+++
@@ -1,8 +0,0 @@
-window.tldreveal = window.tldreveal || {
-  id: 'tldreveal',
-  init: function (deck) {
-     let doctitle = document.head.querySelector('title').text.replace(/\s/g, "-");
-     document.querySelector('.reveal').setAttribute('data-tlid', doctitle);
-     Tldreveal.Tldreveal().init(deck);
-  }
-};
